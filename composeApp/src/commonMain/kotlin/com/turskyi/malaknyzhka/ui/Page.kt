585b97bc2f818cec5b6ad87346e7e918b3f36392
Dmytro Turskyi <dmytro@turskyi.com>
2024-12-26 14:12:50 -0500

    :sparkles: Add vertical scroll to TextPages and background color to Page component
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- composeApp/src/commonMain/kotlin/com/turskyi/malaknyzhka/ui/Page.kt
+++ composeApp/src/commonMain/kotlin/com/turskyi/malaknyzhka/ui/Page.kt
@@ -1,5 +1,6 @@
 package com.turskyi.malaknyzhka.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.turskyi.malaknyzhka.models.PageSettings
 import com.turskyi.malaknyzhka.models.WindowInfo
 import com.turskyi.malaknyzhka.util.rememberWindowSize
@@ -43,7 +45,10 @@ fun Page(pageSettings: PageSettings) {
     val windowInfo: WindowInfo = rememberWindowSize()
 
     BoxWithConstraints(Modifier.fillMaxSize()) {
-        Box(Modifier.fillMaxSize()) {
+        Box(
+            Modifier.fillMaxSize()
+                .background(Color.White)
+        ) {
             BookSpreads(
                 dividerPosition = dividerPosition,
                 currentPage = currentPage,
--- composeApp/src/commonMain/kotlin/com/turskyi/malaknyzhka/ui/TextPages.kt
+++ composeApp/src/commonMain/kotlin/com/turskyi/malaknyzhka/ui/TextPages.kt
@@ -1,13 +1,19 @@
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import malaknyzhka.composeapp.generated.resources.Res
 import malaknyzhka.composeapp.generated.resources._002
 import malaknyzhka.composeapp.generated.resources._003
@@ -18,24 +24,30 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-fun TextPages(
-    currentPage: Int
-) {
+fun TextPages(currentPage: Int) {
     val texts: List<StringResource> = listOf(
         Res.string._002,
         Res.string._003,
         Res.string._004,
     )
 
+    val scrollState: ScrollState = rememberScrollState()
+
     Box(
         Modifier
             .fillMaxWidth()
             .fillMaxHeight()
-            .background(Color.White),
+            .verticalScroll(scrollState)
+            .background(Color.White)
+            .padding(10.dp),
         contentAlignment = Alignment.Center
     ) {
         SelectionContainer {
-            Text(stringResource(texts[currentPage]))
+            Text(
+                text = stringResource(texts[currentPage]),
+                textAlign = TextAlign.Justify
+            )
         }
     }
 }
+
